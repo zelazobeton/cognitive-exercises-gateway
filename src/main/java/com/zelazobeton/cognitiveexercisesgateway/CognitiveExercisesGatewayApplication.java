c69e81e57f90c858dc0f1cd312495f693fed5457
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2022-01-07 14:25:54 +0100

    Initial commit

---
+++ src/main/java/com/zelazobeton/cognitiveexercisesgateway/CognitiveExercisesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zelazobeton.cognitiveexercisesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveExercisesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiveExercisesGatewayApplication.class, args);
+	}
+
+}
