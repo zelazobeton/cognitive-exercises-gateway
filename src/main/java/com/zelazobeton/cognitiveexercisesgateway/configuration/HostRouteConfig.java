031073e193240428052ca743b5961ebe853601c9
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2022-01-08 19:59:52 +0100

    Prepare for running in docker container in prod

--- src/main/java/com/zelazobeton/cognitiveexercisesgateway/configuration/HostRouteConfig.java
+++ src/main/java/com/zelazobeton/cognitiveexercisesgateway/configuration/HostRouteConfig.java
@@ -1,5 +1,6 @@
 package com.zelazobeton.cognitiveexercisesgateway.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Configuration
 public class HostRouteConfig {
 
+    @Value("${cognitive-exercises-main-host}")
+    private String mainMicroserviceHost;
+
+    @Value("${cognitive-exercises-memory-host}")
+    private String memoryMicroserviceHost;
+
     @Bean
     public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/main", "/main/**")
-                        .uri("http://localhost:8081")
+                        .uri(this.mainMicroserviceHost)
                         .id("cognitive-exercises-main"))
                 .route(r -> r.path("/memory", "/memory/**")
-                        .uri("http://localhost:8082")
+                        .uri(this.memoryMicroserviceHost)
                         .id("cognitive-exercises-memory"))
                 .build();
     }
